df14dd78a64e0f8ffdc3bacd2addebea3f27e24b
LeslyeHillary <hillaryconc@gmail.com>
2026-07-10 21:37:57 -0300

    marca a localização do e as medidas do desenho

--- desenho.py
+++ desenho.py
@@ -1,21 +1,21 @@
 #Funções para desenhar
 def desenhar_retangulo(canvas, x1, y1, x2, y2, borda, preenchimento):
     canvas.create_rectangle(
-        x1,
-        y1,
-        x2,
-        y2,
-        outline=borda,
-        fill=preenchimento
+        x1, #coodernada do eixo x inicial
+        y1, #coodernada do eixo y inicial
+        x2, #coodernada do eixo x final
+        y2, #coodernada do eixo y final
+        outline=borda, #cor da borda
+        fill=preenchimento # cor do preenchimento
     )
 
 
 def desenhar_oval(canvas, x1, y1, x2, y2, borda, preenchimento):
     canvas.create_oval(
-        x1,
-        y1,
-        x2,
-        y2,
+        x1, #coodernada do eixo x inicial
+        y1, #coodernada do eixo y inicial
+        x2, #coodernada do eixo x final
+        y2, #coodernada do eixo y final
         outline=borda,
         fill=preenchimento
     )
@@ -23,13 +23,15 @@ def desenhar_oval(canvas, x1, y1, x2, y2, borda, preenchimento):
 
 def desenhar_circulo(canvas, x1, y1, x2, y2, borda, preenchimento):
 
-    raio = min(abs(x2 - x1), abs(y2 - y1))
+    raio = min(abs(x2 - x1), abs(y2 - y1)) #calcula raio utilizando o menor deslocamento
 
-    if x2 < x1:
+    #ajusta a coodernada final do eixo x
+    if x2 < x1: 
         x2 = x1 - raio
     else:
         x2 = x1 + raio
 
+    #ajusta a coodernada final do eixo x
     if y2 < y1:
         y2 = y1 - raio
     else:
